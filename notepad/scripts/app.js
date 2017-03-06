f0c88a02bb07704557c0c1c141797de42ac47b10
Philip Kirkbride <kirkins@gmail.com>
2017-03-06 09:32:10 -0500

    allow multiple Notepad files which, which are each saved seperately

--- notepad/scripts/app.js
+++ notepad/scripts/app.js
@@ -1,5 +1,18 @@
-document.getElementById("content").innerHTML = localStorage["notepad-content"] || "Put something in the body";
+// Function to get url params
+var parseQueryString = function() {
+  var str = window.location.search
+  var objURL = {}
+    str.replace(
+      new RegExp( "([^?=&]+)(=([^&]*))?", "g" ),
+      function( $0, $1, $2, $3 ){
+        objURL[ $1 ] = $3;
+      })
+    return objURL
+}
+params = parseQueryString();
+
+document.getElementById("content").innerHTML = localStorage["notepad-"+params["file"]] || "Put something in the body";
 
 setInterval(function(){
-    localStorage["notepad-content"] = document.getElementById("content").innerHTML;
+    localStorage["notepad-"+params["file"]] = document.getElementById("content").innerHTML;
 },2000)
