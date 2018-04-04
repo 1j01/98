08816790706e0061900aecccc38e4f843d3ee082
Isaiah Odhner <isaiahodhner@gmail.com>
2018-04-04 18:08:28 -0400

    Fix start menu event listeners compounding

--- src/$start-menu.js
+++ src/$start-menu.js
@@ -13,47 +13,32 @@ $start_menu.hide();
 var $start_content_area = $start_menu.find(".all-kinds-of-thing-container");
 var $start_content_default = $start_menu.find(".all-kinds-of-thing-start-content");
 var $windows_cant_do_it_you_can = $start_menu.find(".windows-cant-do-it-you-can");
-var $start_a_program = $start_menu.find(".start-a-program")
+var $start_a_program = $start_menu.find(".start-a-program");
+var $self_help_template = $start_menu.find(".self-help-template");
 var reset_to_start_of_start = function(){
 	$start_content_area.html("").append($start_content_default);
-	$start_content_default.on("click", "a", function(e){
-		e.preventDefault(); // TODO: maybe use the location.hash instead of preventing it
-		var $a = $(e.target);
-		var $section_to_show;
-		if($a.text().match(/program/i)){
-			$section_to_show = $start_a_program;
-		}else{
-			$section_to_show = $windows_cant_do_it_you_can;
-		}
-		$section_to_show.attr({hidden: null});
-		$start_content_area.html("").append($section_to_show);
-		$start_content_area.find(".the-thing-heading").text($a.text());
-		$start_content_area.find(".back-link").on("click", reset_to_start_of_start);
-
-		// TODO: DRY (just hacking this in here real quick)
-		$start_content_area.on("click", "a", function(e){
-			e.preventDefault(); // TODO: maybe use the location.hash instead of preventing it
-			var $a = $(e.target);
-			var $section_to_show;
-			if($a.text().match(/a computer program/i)){
-				$start_menu.hide();
-				$start_button.removeClass("selected");
-				Paint(); // FIXME: doubly executed (presumably other code as well, but most of it is basically idempotent)
-				return;
-			}else if($a.text().match(/a program for people/i)){
-				$section_to_show = $windows_cant_do_it_you_can;
-			}else if($a.text().match(/a program/i)){
-				$section_to_show = $start_a_program;
-			}else{
-				$section_to_show = $windows_cant_do_it_you_can;
-			}
-			$section_to_show.attr({hidden: null});
-			$start_content_area.html("").append($section_to_show);
-			$start_content_area.find(".the-thing-heading").text($a.text());
-			$start_content_area.find(".back-link").on("click", reset_to_start_of_start);
-		});
-	});
 };
+$start_content_area.on("click", "a", function(e){
+	e.preventDefault(); // TODO: maybe use the location.hash for navigation instead of preventing it
+	var $a = $(e.target);
+	var $section_to_show;
+	if($a.text().match(/a computer program/i)){
+		$start_menu.hide();
+		$start_button.removeClass("selected");
+		Paint();
+		return;
+	}else if($a.text().match(/a program for people/i)){
+		$section_to_show = $windows_cant_do_it_you_can;
+	}else if($a.text().match(/a program/i)){
+		$section_to_show = $start_a_program;
+	}else{
+		$section_to_show = $windows_cant_do_it_you_can;
+	}
+	$section_to_show.attr({hidden: null});
+	$start_content_area.html("").append($section_to_show);
+	$start_content_area.find(".the-thing-heading").text($a.text());
+	$start_content_area.find(".back-link").on("click", reset_to_start_of_start);
+});
 reset_to_start_of_start();
 var $start_button = $(".start-button");
 $start_button.on("pointerdown", function(){
