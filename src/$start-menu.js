fec1b02f1275c178f7ebf1dabd9885ae259a0a66
Isaiah Odhner <isaiahodhner@gmail.com>
2018-04-06 21:56:57 -0400

    Close start menu when clicking off

--- src/$start-menu.js
+++ src/$start-menu.js
@@ -40,18 +40,40 @@ $start_content_area.on("click", "a", function(e){
 	$start_content_area.find(".back-link").on("click", reset_to_start_of_start);
 });
 reset_to_start_of_start();
+var open_start_menu = function(){
+	reset_to_start_of_start();
+	$start_button.addClass("selected");
+	$start_menu.attr("hidden", null);
+	$start_menu.slideDown(100); // DOWN AS IN UP (stupid jQuery)
+	$start_menu.css({zIndex: ++$Window.Z_INDEX});
+};
+var close_start_menu = function(){
+	$start_button.removeClass("selected");
+	$start_menu.attr("hidden", "hidden");
+	$start_menu.hide();
+};
+var toggle_start_menu = function(){
+	if($start_menu.is(":hidden")){
+		open_start_menu();
+	}else{
+		close_start_menu();
+	}
+};
+
 var $start_button = $(".start-button");
 $start_button.on("pointerdown", function(){
-	reset_to_start_of_start();
-	if($start_menu.is(":hidden")){
-		$start_button.addClass("selected");
-		$start_menu.attr("hidden", null);
-		$start_menu.slideDown(100); // DOWN AS IN UP (stupid jQuery)
-		$start_menu.css({zIndex: ++$Window.Z_INDEX});
-	}else{
-		$start_button.removeClass("selected");
-		$start_menu.attr("hidden", "hidden");
-		$start_menu.toggle();
+	toggle_start_menu();
+});
+$("body").on("pointerdown", function(e){
+	if($(e.target).closest(".start-menu, .start-button").length === 0){
+		close_start_menu();
 	}
 });
-// TODO: close when you click off
+// Note: A lot of the time it's good to use focusout (in jQuery, or else blur with useCapture?[1]) as opposed to 
+// That might be the case here as well, but maybe not since programs opening might grab focus and that probably shouldn't close the start menu
+// Although at the operating system level it would probably prevent focus switching in the first place, so maybe we could do that
+// The point being this is an operating system control and so it may warrant special handling,
+// but generally I'd recommend making a control focusable and detecting loss of focus as in this answer:
+// [1]: https://stackoverflow.com/a/38317768/2624876
+
+// TODO: Esc to close
