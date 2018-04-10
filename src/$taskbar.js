c023b6bcc5fbeac0ffbab8e3c3a024dcc8baf701
Isaiah Odhner <isaiahodhner@gmail.com>
2018-04-10 14:35:10 -0400

    Show time immediately (remove second long delay)

--- src/$taskbar.js
+++ src/$taskbar.js
@@ -29,8 +29,12 @@ function Task($win){
 	});
 }
 
-var $time = $(".taskbar-time");
-setInterval(function(){
-	$time.text(new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}));
-	$time.attr("title", new Date().toLocaleString([], {weekday: 'long', month: 'long', day: '2-digit', minute: '2-digit', hour: '2-digit'}));
-}, 1000);
+(function(){
+	var $time = $(".taskbar-time");
+	var update_time = function(){
+		$time.text(new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}));
+		$time.attr("title", new Date().toLocaleString([], {weekday: 'long', month: 'long', day: '2-digit', minute: '2-digit', hour: '2-digit'}));
+		setTimeout(update_time, 1000);
+	};
+	update_time();
+}());
