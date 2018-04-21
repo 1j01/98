055379982ef920bb385279364aeea11b9867664c
Isaiah Odhner <isaiahodhner@gmail.com>
2018-04-21 04:28:37 -0400

    Update jspaint
    To https://github.com/1j01/jspaint/tree/474e5658b8796a08a42c617d7fbaa4087fef916f

--- programs/jspaint/lib/konami.js
+++ programs/jspaint/lib/konami.js
@@ -1,55 +1,44 @@
-/**
- * @license konami-js v1.0.1
- * http://mck.me/mit-license
- */
 var Konami = {};
 
-(function(Konami, window) {
-	'use strict';
+(function() {
+	var afterSequence = function(sequence, action) {
+		var index = 0;
 
-	/**
-	 * Creates an event handler responding to the specified sequence.
-	 * @param {...number|function()} arguments
-	 * @return {function(Event)}
-	 */
-	var sequence = Konami.sequence = function() {
-		var sequence = Array.prototype.slice.call(arguments),
-			state = 0;
+		return function(event) {
+			var matchedKey = event.keyCode === sequence[index];
+			// if it didn't match, reset and try matching against the first key
+			if (!matchedKey) {
+				index = 0;
+				matchedKey = event.keyCode === sequence[index];
+			}
 
-		/**
-		 * Event handler
-		 * @param {Event|number} e
-		 */
-		return function(e) {
-			// patch legacy IE
-			e = (e || window.event);
-			e = (e.keyCode || e.which || e);
+			if (matchedKey) {
+				index += 1;
 
-			if (e === sequence[state] || e === sequence[(state = 0)]) {
-				// move next and peek
-				var action = sequence[++state];
-
-				// sequence complete when a function is reached
-				if (typeof action !== 'function') {
-					return;
+				// fix for Firefox with "Search for text when you start typing" enabled
+				// https://support.mozilla.org/en-US/kb/search-contents-current-page-text-or-links
+				// prevent the default (opening Quick Search) for B and A,
+				// which are luckily at the end of the Konami Code sequence
+				// (otherwise it could prevent typing A and B in text fields unwantedly)
+				if (event.keyCode === 66 || event.keyCode === 65) {
+					event.preventDefault();
 				}
 
-				// fire action
-				action();
+				if (index === sequence.length) {
 
-				// reset when sequence completed
-				state = 0;
+					// reset when sequence completed
+					index = 0;
+
+					// fire action
+					action();
+				}
 			}
+
 		};
 	};
 
-	/**
-	 * Creates an event handler responding to the Konami Code.
-	 * @param {function()} action completed sequence callback
-	 * @return {function(Event)}
-	 */
 	Konami.code = function(action) {
-		return sequence(38, 38, 40, 40, 37, 39, 37, 39, 66, 65, action);
+		return afterSequence([38, 38, 40, 40, 37, 39, 37, 39, 66, 65], action);
 	};
 
-})(Konami, window);
+}());
