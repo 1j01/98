5ef212ed54cea620fed85bba3f460757ddbe0613
Isaiah Odhner <isaiahodhner@gmail.com>
2019-12-29 19:15:59 -0500

    DRY

--- pull-libs.js
+++ pull-libs.js
@@ -1,18 +1,20 @@
 // TODO: include jquery and stuff; maybe even update script tags in html, with CDN URLs, integrity checksums, and fallbacks
 
 const {copySync} = require("fs-extra");
+const {basename, join} = require("path");
 
-const copySyncAndLog = (from, to)=> {
+const copy = (from, toDir)=> {
+	const to = join(toDir, basename(from));
 	console.log(`Copying ${from} => ${to}`);
 	copySync(from, to);
 };
 
-copySyncAndLog("node_modules/webamp/built/webamp.bundle.js", "programs/winamp/lib/webamp.bundle.js");
-copySyncAndLog("node_modules/webamp/built/webamp.bundle.min.js", "programs/winamp/lib/webamp.bundle.min.js");
-copySyncAndLog("node_modules/browserfs/dist/browserfs.js", "lib/browserfs.js");
-copySyncAndLog("node_modules/browserfs/dist/browserfs.min.js", "lib/browserfs.min.js");
-copySyncAndLog("node_modules/butterchurn/lib/butterchurn.min.js", "programs/winamp/lib/butterchurn.min.js");
-copySyncAndLog("node_modules/butterchurn-presets/lib/butterchurnPresets.min.js", "programs/winamp/lib/butterchurnPresets.min.js");
+copy("node_modules/webamp/built/webamp.bundle.js", "programs/winamp/lib/");
+copy("node_modules/webamp/built/webamp.bundle.min.js", "programs/winamp/lib/");
+copy("node_modules/browserfs/dist/browserfs.js", "lib/");
+copy("node_modules/browserfs/dist/browserfs.min.js", "lib/");
+copy("node_modules/butterchurn/lib/butterchurn.min.js", "programs/winamp/lib/");
+copy("node_modules/butterchurn-presets/lib/butterchurnPresets.min.js", "programs/winamp/lib/");
 console.log(`Done.
 
-Note: This script doesn't handle updating npm dependencies, just copying scripts into the repo.`);
+Note: This script doesn't handle updating npm dependencies, it just copies scripts into the repo.`);
