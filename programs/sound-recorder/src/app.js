2c3a8edc5aef96908da92054843404faf517947c
Isaiah Odhner <isaiahodhner@gmail.com>
2020-04-02 23:22:46 -0400

    Do away with this silly setup.js

--- programs/sound-recorder/src/app.js
+++ programs/sound-recorder/src/app.js
@@ -1,4 +1,15 @@
 
+// webkit shim
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+window.URL = window.URL || window.webkitURL;
+
+if(window.AudioContext){
+	var audio_context = new AudioContext;
+}else{
+	alert('No web audio support in this browser!');
+}
+
 var previous_time;
 var tid = -1;
 var recording = false;
