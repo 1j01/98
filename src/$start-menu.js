98b22fe185b509c0f8f85362b85636d5dee3b712
Isaiah Odhner <isaiahodhner@gmail.com>
2020-04-08 01:15:20 -0400

    Change start menu gag

--- src/$start-menu.js
+++ src/$start-menu.js
@@ -10,38 +10,7 @@ var username = username_match && username_match[1] || "Admin";
 var $start_menu = $(".start-menu");
 $start_menu.hide();
 // TODO: legitimate contents or whatever
-var $start_content_area = $start_menu.find(".all-kinds-of-thing-container");
-var $start_content_default = $start_menu.find(".all-kinds-of-thing-start-content");
-var $windows_cant_do_it_you_can = $start_menu.find(".windows-cant-do-it-you-can");
-var $start_a_program = $start_menu.find(".start-a-program");
-var $self_help_template = $start_menu.find(".self-help-template");
-var reset_to_start_of_start = function(){
-	$start_content_area.html("").append($start_content_default);
-};
-$start_content_area.on("click", "a", function(e){
-	e.preventDefault(); // TODO: maybe use the location.hash for navigation instead of preventing it
-	var $a = $(e.target);
-	var $section_to_show;
-	if($a.text().match(/a computer program/i)){
-		$start_menu.hide();
-		$start_button.removeClass("selected");
-		Paint();
-		return;
-	}else if($a.text().match(/a program for people/i)){
-		$section_to_show = $windows_cant_do_it_you_can;
-	}else if($a.text().match(/a program/i)){
-		$section_to_show = $start_a_program;
-	}else{
-		$section_to_show = $windows_cant_do_it_you_can;
-	}
-	$section_to_show.attr({hidden: null});
-	$start_content_area.html("").append($section_to_show);
-	$start_content_area.find(".the-thing-heading").text($a.text());
-	$start_content_area.find(".back-link").on("click", reset_to_start_of_start);
-});
-reset_to_start_of_start();
 var open_start_menu = function(){
-	reset_to_start_of_start();
 	$start_button.addClass("selected");
 	$start_menu.attr("hidden", null);
 	$start_menu.slideDown(100); // DOWN AS IN UP (stupid jQuery)
