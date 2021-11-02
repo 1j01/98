12d0899b55b1bec0f1130180177ac8b466567d31
Isaiah Odhner <isaiahodhner@gmail.com>
2021-11-01 22:33:46 -0400

    Animate wallpaper with feDisplacementMap
    I was originally trying to implement the science wallpaper,
    but it didn't work in chrome. Instead I got an interesting animation effect for the wallpaper,
    where it seems to use the SourceGraphic as the displacement map when applying the filter to an HTML element.
    Displacing by the input gives a nice flow to the animation, but there's two problems:
    1. The animation breaks down (and gets laggy) over time, and can't loop nicely.
    2. This only works in Chrome, due to the buggy behavior. Could I even get it to work cross browser??

--- src/$desktop.js
+++ src/$desktop.js
@@ -57,3 +57,87 @@ $("html").on("drop", function (event) {
 $(window).on("scroll focusin", () => {
 	window.scrollTo(0, 0);
 });
+
+// SVG filter for animating the desktop background
+
+const map_url = "images/swirl.webp";
+const displacement_scale = 0;
+const svg_source = `
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		id="wallpaper-distortion-filter-svg"
+		width="0" height="0"
+		style="position: absolute;"
+	><defs>
+		<filter
+			id="wallpaper-distortion"
+			width="1" height="1" x="0" y="0"
+			color-interpolation-filters="sRGB"
+			filterUnits="objectBoundingBox"
+			preserveAspectRatio="none"
+		>
+			<feFlood
+				width="100%" height="100%"
+				x="0" y="0"
+				flood-color="rgb(127,127,127)"
+				
+				result="identity-displacement"
+			/>
+			<feImage
+				width="100%" height="100%"
+				x="0.1" y="0.1"
+				href="${map_url}"
+				preserveAspectRatio="xMidYMid"
+				result="displacement-map-image"
+			/>
+			<feMerge result="displacement-map">
+				<feMergeNode in="identity-displacement"/>
+				<feMergeNode in="displacement-map-image"/>
+			</feMerge>
+			<feDisplacementMap
+				in="SourceGraphic"
+				in2="displacement-map"
+				scale="${displacement_scale}"
+				xChannelSelector="R"
+				yChannelSelector="G"
+				result="displaced"
+			>
+				<animate
+				attributeName="scale" from="0" to="500"
+				dur="50s" repeatCount="indefinite" />
+			</feDisplacementMap>
+			<feComponentTransfer in="displacement-map" result="displacement-debug">
+			  <feFuncA type="linear" slope="0" id='displacement-debug-feFuncA'/>
+			</feComponentTransfer>
+			<feMerge>
+				<feMergeNode in="displaced"/>
+				<feMergeNode in="displacement-debug"/>
+			</feMerge>
+		</filter>	
+	</defs></svg>
+`;
+let svg = document.getElementById("wallpaper-distortion-filter-svg");
+if (!svg) {
+	var doc = new DOMParser().parseFromString(svg_source, "application/xml");
+	document.body.appendChild(document.importNode(doc.querySelector("svg"), true));
+}
+document.querySelector(".wallpaper").style.filter = "url(#wallpaper-distortion)";
+addEventListener("keydown", (event) => {
+	if (event.key === "d") {
+		const feFuncA = document.getElementById("displacement-debug-feFuncA");
+		if (feFuncA.getAttribute("slope") === "0") {
+			feFuncA.setAttribute("slope", "0.5");
+		} else if (feFuncA.getAttribute("slope") === "0.5") {
+			feFuncA.setAttribute("slope", "1");
+		} else {
+			feFuncA.setAttribute("slope", "0");
+		}
+	}
+});
+// addEventListener("mousemove", (event) => {
+// 	document.querySelector("#wallpaper-distortion-filter-svg feDisplacementMap").setAttribute("scale",
+// 		(event.clientX / window.innerWidth) * 1000
+// 	);
+// });
+console.log(document.querySelector("feImage").href);
