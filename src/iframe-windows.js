1665da8de504b22217daa4015f57134148a6408b
Isaiah Odhner <isaiahodhner@gmail.com>
2021-11-16 17:20:25 -0500

    Fix Shift+Alt+Tab (it wasn't detecting releasing Alt while holding Shift)

--- src/iframe-windows.js
+++ src/iframe-windows.js
@@ -158,7 +158,8 @@ function make_iframe_window(options) {
 	}
 	function handle_keyup(e) {
 		// console.log("keyup", e.key, e.code);
-		if (e.key === "Alt") {
+		// if (e.key === "Alt") { // on my Ubuntu XFCE, it's giving "Meta" if Shift is held
+		if (!e.altKey) {
 			alt_held = false;
 			clearInterval(iid);
 			// console.log("Alt released");
