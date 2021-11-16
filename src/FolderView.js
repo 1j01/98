30e613b1fe57958e957819da9c34df7e9ec46ba8
Isaiah Odhner <isaiahodhner@gmail.com>
2021-11-16 00:44:09 -0500

    Implement searching and cycling items in folder view by typing

--- src/FolderView.js
+++ src/FolderView.js
@@ -541,7 +541,12 @@ function FolderView(folder_path, { asDesktop = false, onStatus, openFolder, open
 		};
 	})();
 
+	let search_string = "";
+	let search_timeout;
+
 	$folder_view.on("keydown", function (e) {
+		// console.log("keydown", e.isDefaultPrevented());
+
 		if (e.target.tagName === "INPUT" || e.target.tagName === "TEXTAREA") {
 			return;
 		}
@@ -596,7 +601,7 @@ function FolderView(folder_path, { asDesktop = false, onStatus, openFolder, open
 		} else if (e.key == "End") {
 			e.preventDefault();
 			select_item(self.items[self.items.length - 1], e);
-		} else if (e.key == " ") {
+		} else if (e.key == " " && search_string.length === 0) {
 			// Usually there's something focused,
 			// so this is pretty "niche", but space bar selects the focused item.
 			// Ctrl+Space toggles selection of the focused item.
@@ -610,6 +615,48 @@ function FolderView(folder_path, { asDesktop = false, onStatus, openFolder, open
 		} else if (e.key === "F2") {
 			e.preventDefault();
 			self.start_rename();
+		} else {
+			if (e.isDefaultPrevented() || e.ctrlKey || e.altKey || e.metaKey) {
+				return;
+			}
+			if (search_timeout) {
+				clearTimeout(search_timeout);
+			}
+			if (search_string === e.key) {
+				// cycle through items starting with the same letter
+				// Note: not adding to search_string here, so it stays as e.key
+				// @TODO: what if you have an item like "Llama Photos", can you not search for "Llama" to go to it, in the presence of other 'L' items?
+				const candidates = self.items.filter((item) => {
+					const title = item.element.querySelector(".title").textContent; // @TODO: proper access
+					return title.toLocaleLowerCase().startsWith(search_string.toLocaleLowerCase());
+				});
+				if (candidates.length > 0) {
+					const index = candidates.findIndex((item) => item.element.classList.contains("focused"));
+					if (index === -1) {
+						select_item(candidates[0], e);
+					} else {
+						select_item(candidates[(index + 1) % candidates.length], e);
+					}
+				}
+			} else {
+				// focus item matching search string
+				if (e.key !== "Shift" && e.key !== "Compose") { // Note: composition doesn't actually work; I'd need an input element to do this properly
+					search_string += e.key;
+				}
+				// console.log("search_string: " + search_string);
+				search_timeout = setTimeout(function () {
+					search_string = "";
+					// console.log("reset search_string");
+				}, 1000);
+
+				for (const item of self.items) {
+					const title = item.element.querySelector(".title").textContent; // @TODO: proper access
+					if (title.toLocaleLowerCase().startsWith(search_string.toLocaleLowerCase())) {
+						select_item(item, {}); // passing fake event so it doesn't use shiftKey to determine multi-select
+						break;
+					}
+				}
+			}
 		}
 	});
 
