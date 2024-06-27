66acb744668e666cec4a103731f90a4c2efd6bed
Isaiah Odhner <isaiahodhner@gmail.com>
2024-06-27 15:27:14 -0400

    Avoid an error I saw at least once
    If the iframe document is unloaded, the cleanup shouldn't be needed.

--- src/iframe-windows.js
+++ src/iframe-windows.js
@@ -47,7 +47,7 @@ function enhance_iframe(iframe) {
 			iframe.contentDocument.addEventListener("mouseup", clean_up_delegation);
 			function clean_up_delegation() {
 				$G.off("mouseup blur", delegate_pointerup);
-				iframe.contentDocument.removeEventListener("mouseup", clean_up_delegation);
+				iframe.contentDocument?.removeEventListener("mouseup", clean_up_delegation);
 			}
 		});
 
