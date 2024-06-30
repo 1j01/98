ecac20eb617335233bf11eba7a62b399611ccd6c
Isaiah Odhner <isaiahodhner@gmail.com>
2024-06-30 18:00:42 -0400

    Test adding 1+2 in Calculator

---
+++ tests/calculator.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+	await page.goto('http://localhost:1998/');
+	await page.getByText('Calculator').dblclick();
+});
+
+test('can do some math', async ({ page }) => {
+	await page.frameLocator('iframe').getByRole('button', { name: '1' }).click();
+	await page.frameLocator('iframe').getByLabel('add').click();
+	await page.frameLocator('iframe').getByRole('button', { name: '2' }).click();
+	await page.frameLocator('iframe').getByLabel('equals').click();
+	await expect(page.frameLocator('iframe').getByRole('textbox')).toHaveValue('3');
+});
