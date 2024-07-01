4d21cae1402b902b96016b5d381f8c4e5458d13b
Isaiah Odhner <isaiahodhner@gmail.com>
2024-07-01 00:47:17 -0400

    Disable Calculator test in WebKit

--- tests/calculator.spec.ts
+++ tests/calculator.spec.ts
@@ -5,7 +5,9 @@ test.beforeEach(async ({ page }) => {
 	await page.getByText('Calculator').dblclick();
 });
 
-test('can do some math', async ({ page }) => {
+test('can do some math', async ({ page, browserName }) => {
+	// TODO: test error handling in case WebAssembly isn't supported
+	test.skip(browserName === 'webkit', 'WebAssembly not supported in WebKit');
 	await page.frameLocator('iframe').getByRole('button', { name: '1' }).click();
 	await page.frameLocator('iframe').getByLabel('add').click();
 	await page.frameLocator('iframe').getByRole('button', { name: '2' }).click();
