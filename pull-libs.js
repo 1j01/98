83e0cfdccab780f6329ac563ebeffa50a2de8c15
Isaiah Odhner <isaiahodhner@gmail.com>
2024-07-02 00:49:56 -0400

    Build ZenFS manually

--- pull-libs.js
+++ pull-libs.js
@@ -1,6 +1,6 @@
 // TODO: include jquery and stuff; maybe even update script tags in html, with CDN URLs, integrity checksums, and fallbacks
 
-const { copyFileSync, mkdirSync } = require("fs");
+// const { copyFileSync, mkdirSync } = require("fs");
 // TODO: probably don't need fs-extra these days, could use fs.cp/copyFile instead
 const { copySync, readFileSync, writeFileSync } = require("fs-extra");
 const { basename, join } = require("path");
@@ -10,11 +10,11 @@ const copy = (from, toDir) => {
 	console.log(`Copying ${from} => ${to}`);
 	copySync(from, to);
 };
-const copyFile = (fromFile, toFile) => {
-	console.log(`Copying ${fromFile} => ${toFile}`);
-	mkdirSync(join(toFile, ".."), { recursive: true });
-	copyFileSync(fromFile, toFile);
-};
+// const copyFile = (fromFile, toFile) => {
+// 	console.log(`Copying ${fromFile} => ${toFile}`);
+// 	mkdirSync(join(toFile, ".."), { recursive: true });
+// 	copyFileSync(fromFile, toFile);
+// };
 
 copy("node_modules/os-gui/build/windows-98.css", "lib/os-gui/");
 copy("node_modules/os-gui/build/windows-98.css.map", "lib/os-gui/");
@@ -28,10 +28,15 @@ copy("node_modules/os-gui/parse-theme.js", "lib/os-gui/");
 copy("node_modules/os-gui/$MenuBar.js", "lib/os-gui/");
 copy("node_modules/os-gui/MenuBar.js", "lib/os-gui/");
 copy("node_modules/os-gui/$Window.js", "lib/os-gui/");
-copyFile("node_modules/@zenfs/core/dist/browser.min.js", "lib/zenfs/zenfs-browser.min.js");
-// I renamed the JS file, but it will still point to the original map file
-// unless I patch it like I did with Webamp
-copyFile("node_modules/@zenfs/core/dist/browser.min.js.map", "lib/zenfs/browser.min.js.map");
+
+// For ZenFS, I had to build it manually with `target: 'es2022'` in `scripts/build.js`
+// since it's using `using`, a bleeding edge keyword for resource management.
+// While I was at it, I changed the name to `zenfs-browser.js` and disabled minification.
+// copyFile("node_modules/@zenfs/core/dist/browser.min.js", "lib/zenfs/zenfs-browser.min.js");
+// // I renamed the JS file, but it will still point to the original map file
+// // unless I patch it like I did with Webamp
+// copyFile("node_modules/@zenfs/core/dist/browser.min.js.map", "lib/zenfs/browser.min.js.map");
+
 // copy("node_modules/webamp/built/webamp.bundle.js", "programs/winamp/lib/");
 // copy("node_modules/webamp/built/webamp.bundle.min.js", "programs/winamp/lib/");
 // copy("node_modules/webamp/built/webamp.bundle.min.js.map", "programs/winamp/lib/");
